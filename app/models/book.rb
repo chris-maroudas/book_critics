4c65a829f509826a168d868f5413f3263913cae2
Chris Maroudas <mallibu97@gmail.com>
2014-09-08 22:29:27 +0300

    Add elastic search to Book model and annotated every model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,21 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'elasticsearch/model'
+
 class Book < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
   has_many :reviews
 
 end
+
+Book.import # for auto sync model with elastic search
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#  rating     :integer
+#  book_id    :integer
+#  user_id    :integer
+#
+
 class Review < ActiveRecord::Base
 
   belongs_to :user
