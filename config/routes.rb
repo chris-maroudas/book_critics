9da72751a68a1a4982598c52ee8387325c1a2ffb
Chris Maroudas <mallibu97@gmail.com>
2014-09-08 22:29:38 +0300

    Add search route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  # Search
+  get 'search', to: 'search#search'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
